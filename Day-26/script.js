5909a0dec2680599ff4af262ba3d416da3e98565
NEHA REDDY K <nehaareddy02@gmail.com>
2026-03-04 10:01:02 +0530

    🚀 Day 26 - index.html - 10:00:59 AM

---
+++ Day-26/script.js
@@ -0,0 +1,7 @@
+// Day 26 Logic Practice
+console.log('Lab Session 26 Started');
+
+const display = document.getElementById('display');
+display.innerText = 'Logic Engine Online';
+
+// Practice your JS code here...
